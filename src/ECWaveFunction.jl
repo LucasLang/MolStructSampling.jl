42e43a732b5293f2a0aee93fa942ff7c58410205
Lucas Lang <lucas.lang@kjemi.uio.no>
2023-01-16 07:31:16 +0100

    Introduced a new struct for processed wavefunction parameters, started writing its constructor.

--- src/ECWaveFunction.jl
+++ src/ECWaveFunction.jl
@@ -2,12 +2,26 @@ module ECWaveFunction
 
 export WaveFuncParam, flattened_to_lower, flattened_to_symmetric
     
+struct PseudoParticlePermutation
+    transpositions::Vector{Tuple{Int64, Int64}}
+end
+
 """
-   n: Number of quasiparticles (= one less than the number of actual particles)
-   M: Number of explicitly correlated basis functions
-   C: The vector of linear expansion coefficients
-   L_flattened: For each of the M basis functions: vector of n(n+1)/2 elements of the lower-triangular Cholesky factor L
-   B_flattened: For each of the M basis functions: vector of n(n+1)/2 unique elements of the symmetric matrix B
+    coeffs: prefactors of the permutation operators
+    permutations: actual permutations whose linear combination is taken
+"""
+struct YoungOperator
+    coeffs::Vector{Float64}
+    permutations::Vector{PseudoParticlePermutation}
+end
+
+"""
+    n: Number of quasiparticles (= one less than the number of actual particles)
+    M: Number of explicitly correlated basis functions
+    C: The vector of linear expansion coefficients
+    L_flattened: For each of the M basis functions: vector of n(n+1)/2 elements of the lower-triangular Cholesky factor L
+    B_flattened: For each of the M basis functions: vector of n(n+1)/2 unique elements of the symmetric matrix B
+    Y: The product of Young operators (projector applied to trial wavefunction)
 
 """
 struct WaveFuncParam
@@ -16,10 +30,42 @@ struct WaveFuncParam
     C::Vector{Float64}
     L_flattened::Vector{Vector{Float64}}
     B_flattened::Vector{Vector{Float64}}
+    Y::YoungOperator
 end
 
-struct PseudoParticlePermutation
-    transpositions::Vector{Tuple{Int64, Int64}}
+"""
+    n: Number of quasiparticles (= one less than the number of actual particles)
+    M: Number of explicitly correlated basis functions
+    C: The vector of linear expansion coefficients
+    A[k, i]: A matrix of the ith permutation applied to the kth basis function
+    B[k, i]: B matrix of the ith permutation applied to the kth basis function
+    perm_coeffs: The coefficients of the permutations in the Young operator
+"""
+struct WaveFuncParamProcessed
+    n::Int64
+    M::Int64
+    C::Vector{Float64}
+    A::Matrix{Matrix{Float64}}
+    B::Matrix{Matrix{Float64}}
+    perm_coeffs::Vector{Float64}
+end
+
+function WaveFuncParamProcessed(param::WaveFuncParam)
+    n = param.n
+    M = param.M
+    C = param.C
+    perm_len = length(Y.coeffs)
+    A = Matrix{Matrix{Float64}}(undef, M, perm_len)
+    B = Matrix{Matrix{Float64}}(undef, M, perm_len)
+    for k in 1:M
+        for i in 1:perm_len
+            L = flattened_to_lower(n, param.L_flattened[k])
+            A = L*L'
+            B = flattened_to_symmetric(n, param.B_flattened[k])
+            A[k, i] = CONTINUE LATER
+            B[k, i] = CONTINUE LATER
+        end
+    end
 end
 
 """
